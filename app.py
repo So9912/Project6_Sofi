60d6e5214abb688b604731f9b76f1f3773cde382
Sofia Lopez Pavon <crow921@hotmail.com>
2024-08-12 19:24:09 -0600

    Aguegue la lógica de los graficos

---
+++ app.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv ('vehicles_us.csv')
+
+print(car_data)
+
+hist_button = st.button('Construir Histograma')
+scatter_plot_button = st.button('Construir grafico de dispersion')
+
+if hist_button:
+    st.write('Creacion de un histograma para el conjunto de datos.')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width = True)
+    
+if scatter_plot_button:
+    st.write('Creacion de un grafico de dispersion para el conjunto de datos.')
+    fig = px.scatter(car_data, x="odometer", y="price", title='Gráfico de dispersión entre odómetro y precio')
+    st.plotly_chart(fig, use_container_width=True)
